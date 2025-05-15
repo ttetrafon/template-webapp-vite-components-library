e86e18414701964d08788697da325b6ce58a91f0
ttetrafon <info@ttetrafon.com>
2025-05-16 00:07:18 +0300

    split enums between local & library

--- editable-page.js
+++ editable-page.js
@@ -1,7 +1,7 @@
 import isEqual from 'lodash/isEqual';
 import styles from '../style.css?inline';
 import state from '../services-library/state.js';
-import { eventNames, messageTypes } from '../data/enums.js';
+import { eventNames, messageTypes } from '../data-library/enums.js';
 import { buildHtmlFromStructure, buildStructureFromHtml, setCaretPosition, setCaretPositionAtEnd } from '../helper-library/dom.js';
 import { Command_Editor_UpdateLines } from '../model/command.js';
 
--- input-field.js
+++ input-field.js
@@ -1,5 +1,5 @@
 import styles from '../style.css?inline';
-import { eventNames } from '../data/enums.js';
+import { eventNames } from '../data-library/enums.js';
 import { emitCustomEvent } from '../helper-library/dom.js';
 
 const template = document.createElement('template');
